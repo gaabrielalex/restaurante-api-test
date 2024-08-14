25eab71828708a1daae541b4cf39111070bb18ce
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-08-13 22:49:01 -0400

    .

---
+++ src/main/java/ifmt/cba/utils/ApiUtils.java
@@ -0,0 +1,6 @@
+package ifmt.cba.utils;
+
+public class ApiUtils {
+
+    public static final String urlBase = "http://localhost:8080";
+}
--- src/test/java/ifmt/cba/ProdutoServicoTest1.java
+++ src/test/java/ifmt/cba/ProdutoServicoTest1.java
@@ -2,18 +2,68 @@ package ifmt.cba;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.google.gson.Gson;
 
 import ifmt.cba.dto.GrupoAlimentarDTO;
 import ifmt.cba.dto.ProdutoDTO;
+import ifmt.cba.utils.ApiUtils;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import java.time.Instant;
 
 public class ProdutoServicoTest1 {
+
+    @BeforeAll
+    public static void setup() {
+        // Configuração básica para o Rest Assured
+        RestAssured.baseURI = ApiUtils.urlBase;
+        RestAssured.basePath = "/produto";
+    }
+
+    @Test
+    public void aoAdicionarProduto_DeveRetornarRespostaComStatus200ECComDadosDeRepostaCorretos() {
+        GrupoAlimentarDTO grupoAlimentarDTO = new GrupoAlimentarDTO();
+        try {
+            grupoAlimentarDTO = obterGrupoAlimentarValido();
+
+        } catch (Exception e) {
+            Assertions.fail("Erro ao obter grupo alimentar válido");
+        }
+
+        ProdutoDTO produtoInserido = new ProdutoDTO();
+        produtoInserido.setNome("Alcatra bovina - " + Instant.now());
+        produtoInserido.setCustoUnidade(10.0f);
+        produtoInserido.setValorEnergetico(200);
+        produtoInserido.setEstoque(100);
+        produtoInserido.setEstoqueMinimo(10);
+        produtoInserido.setGrupoAlimentar(grupoAlimentarDTO);
+
+        RestAssured
+            .given()
+                .log().all()
+                .contentType("application/json")
+                .body(produtoInserido)
+            .when()
+                .post()
+            .then()
+                .log().all()
+                .statusCode(200)
+                .body("codigo", Matchers.is(Matchers.notNullValue()))
+                .body("codigo", Matchers.greaterThan(0))
+                .body("nome", Matchers.is(produtoInserido.getNome()))
+                .body("custoUnidade", Matchers.is(produtoInserido.getCustoUnidade()))
+                .body("valorEnergetico", Matchers.is(produtoInserido.getValorEnergetico()))
+                .body("estoque", Matchers.is(produtoInserido.getEstoque()))
+                .body("estoqueMinimo", Matchers.is(produtoInserido.getEstoqueMinimo()))
+                .body("grupoAlimentar.codigo", Matchers.is(produtoInserido.getGrupoAlimentar().getCodigo()))
+                .body("link", Matchers.is(Matchers.notNullValue()))
+                .body("link", Matchers.is(Matchers.not(Matchers.emptyString())));
+    }
 
     @Test
     public void testConsultarPorCodigoVerificandoValores1() {
@@ -94,5 +144,15 @@ public class ProdutoServicoTest1 {
                 .statusCode(200)
                 .body("codigo", Matchers.is(Matchers.notNullValue()))
                 .body("estoque", Matchers.is(estoqueAtual-20));
+    }
+
+    public static GrupoAlimentarDTO obterGrupoAlimentarValido() throws Exception {
+        Response response = RestAssured.request(Method.GET, ApiUtils.urlBase + "/grupoalimentar/codigo/1");
+        if (response.getStatusCode() != 200) {
+            throw new Exception("Erro ao obter grupo alimentar válido"); 
+        } else {
+            Gson gson = new Gson();
+            return gson.fromJson(response.getBody().asString(), GrupoAlimentarDTO.class);
+        }
     }
 }
